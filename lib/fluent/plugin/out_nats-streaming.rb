7b83197332a046ae988684dcc9a4568f0b8165a5
HC Chien <hc.chien@pentium.net>
2019-11-25 10:53:11 +0800

    fix out_nats-streaming.rb

--- lib/fluent/plugin/out_nats-streaming.rb
+++ lib/fluent/plugin/out_nats-streaming.rb
@@ -1,12 +1,15 @@
 require "fluent/plugin/output"
-require "nats/client"
+# require "nats/client"
+require 'stan/client'
 
 module Fluent::Plugin
-  class NatsStreamingOutput < Fluent::Plugin::Output
+  class NatsStreamingOutput < Output
+
     Fluent::Plugin.register_output('nats-streaming', self)
 
-    helpers :formatter, :thread, :inject
+    helpers :formatter, :thread, :inject, :compat_parameters
 
+    DEFAULT_BUFFER_TYPE = "memory"
     DEFAULT_FORMAT_TYPE = 'json'
 
     config_param :server, :string, :default => 'localhost:4222',
@@ -24,14 +27,18 @@ module Fluent::Plugin
 
     config_param :max_reconnect_attempts, :integer, :default => 150,
                  :desc => "The max number of reconnect tries"
-    config_param :reconnect_time_wait, :integer, default: 2
+    config_param :reconnect_time_wait, :integer, :default => 2,
                  :desc => "The number of seconds to wait between reconnect tries"
 
     config_section :buffer do
-      config_set_default :chunk_keys, ["topic"]
+      config_set_default :chunk_keys, ['tag']
+      config_set_default :flush_at_shutdown, true
+      config_set_default :chunk_limit_size, 10 * 1024
     end
+
     config_section :format do
-      config_set_default :@type, 'json'
+      config_set_default :@type, DEFAULT_FORMAT_TYPE
+      config_set_default :add_newline, false
     end
 
     def initialize
@@ -50,13 +57,9 @@ module Fluent::Plugin
       super
 
       @sc_config = {
-          servers: ["nats://#{server}"]
-          #uri: "nats://#{@host}:#{@port}",
-          #ssl: @ssl,
-          #user: @user,
-          #pass: @password,
+          servers: ["nats://#{server}"],
           reconnect_time_wait: @reconnect_time_wait,
-          max_reconnect_attempts: @max_reconnect_attempts,
+          max_reconnect_attempts: @max_reconnect_attempts
         }
       @formatter = formatter_create
     end
@@ -76,21 +79,14 @@ module Fluent::Plugin
       @sc = nil
     end
 
-#      def shutdown
-#        EM.next_tick do
-#          NATS.stop
-#        end
-#        super
-#      end
-
     def run
       begin
+        log.info "connect nats server nats://#{server} #{cluster_id} #{client_id}"
         @sc = STAN::Client.new
         @sc.connect(@cluster_id, @client_id, @sc_config)
       rescue Exception => e
         log.warn "Send exception occurred: #{e}"
         log.warn "Exception Backtrace : #{e.backtrace.join("\n")}"
-        log.warn "Exception ignored in tag : #{tag}" if ignore
         raise
       end
     end
@@ -103,7 +99,9 @@ module Fluent::Plugin
     end
 
     def format(tag, time, record)
-      @formatter.format(tag, time, record)
+      record = inject_values_to_record(tag, time, record)
+      @formatter.format(tag, time, record).chomp + "\n"
     end
+
   end
 end
